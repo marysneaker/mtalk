692079dba919a38b6b3ff49d5c7f5379df772549
sinoswr <sinoswr@jeffersontekimacbook-pro.local>
2015-12-01 15:06:22 +0800

    Initial Commit

---
+++ mtalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mtalk
+//
+//  Created by sinoswr on 15/12/1.
+//  Copyright © 2015年 sinoswr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mtalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mtalk
+//
+//  Created by sinoswr on 15/12/1.
+//  Copyright © 2015年 sinoswr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
